799b4e863ea23965bca85ded9543d8751ac7987f
Dmitriy Tsurkan <dmitriy_tsurkan@ymail.com>
2017-09-29 17:38:36 +0300

    first commit

---
+++ Cities/Common/Services/Networking/WeatherAPI.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherAPI.swift
+//  Cities
+//
+//  Created by Vadym Riznychok on 9/28/17.
+//  Copyright © 2017 Vadym Riznychok. All rights reserved.
+//
+
+import Foundation
---
+++ Cities/Modules/CitiesList/Subviews/CityCell.swift
@@ -0,0 +1,24 @@
+//
+//  CityCell.swift
+//  Cities
+//
+//  Created by Vadym Riznychok on 9/28/17.
+//  Copyright © 2017 Vadym Riznychok. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
